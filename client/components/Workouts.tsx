aca358b8246f0f97efd45a1f82ca9fad7b46f41b
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-09-25 18:22:22 -0700

    basic styling for app, configured react calendar

--- client/components/Workouts.tsx
+++ client/components/Workouts.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+// import './Sample.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const Workouts = () => {
   console.log('hi from workouts');
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  // const invalidValue: string = selectedDate;
+  const [value, onChange] = useState<Value>(new Date());
+  // const [date, setDate] = useState<Value>(new Date());
+  console.log('value:', value);
   return (
-    <div>
-      <button>
+    <div className='flex flex-col'>
+      <button className="bg-blue-500 text-white font-bold ">
+        {/* {value} */}
+        Current Date
+      </button>
+      <button className="bg-green-500 text-white font-bold py-2 px-4 rounded">
         +
       </button>
+
+      <div className="calendar-container">
+        {/* <main className="Sample__container__content"> */}
+        <Calendar onChange={onChange} showWeekNumbers value={value} />
+        {/* </main> */}
+      </div>
     </div>
   );
+
 };
 
 export default Workouts;
--- client/containers/NavBar.tsx
+++ client/containers/NavBar.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const NavBar = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className='flex justify-around items-end border-4 border-indigo-500'>
       <button onClick={() => {navigate('/Workouts');}}>Workouts</button>
       <button onClick={() => {navigate('/Progress');}}>Progress</button>
     </div>
